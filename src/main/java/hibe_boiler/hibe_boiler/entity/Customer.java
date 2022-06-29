398f0c9edd92dde5baad1e076db271399d0131f4
pillai_amal <mail.vortextv@gmail.com>
2022-06-29 11:14:38 +0530

    fixed the customer class

--- src/main/java/hibe_boiler/hibe_boiler/entity/Customer.java
+++ src/main/java/hibe_boiler/hibe_boiler/entity/Customer.java
@@ -1,11 +1,10 @@
 package hibe_boiler.hibe_boiler.entity;
 import java.time.LocalDate; 
-
 import jakarta.persistence.*;
-public class Customer {
-	@Entity
-	@Table(name = "customer")
-	public class CustomerEntity {
+
+@Entity
+@Table(name = "customer") 
+public class Customer { 
 		@Id
 		private Integer customerId;
 		private String customerName;
@@ -13,6 +12,23 @@ public class Customer {
 		private String address;
 		@Column(name = "phoneNumber")
 		private Long contactNumber;
+		
 	  //getters & setters
-	}
+        
+		public void setCustomerId(Integer customerId) {
+            this.customerId = customerId;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public void setDateOfBirth(LocalDate dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public void setAddress(String address) {
+            this.address = address; 
+        }
+
 }
